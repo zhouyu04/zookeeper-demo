bca79bd9da5ed27b47705f171dad9e72c956cfba
周宇 <yu_z@kingdee.com>
2021-02-01 18:01:43 +0800

    init boot2

---
+++ src/test/java/com/zzyy/zookeeperdemo/ZookeeperDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzyy.zookeeperdemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZookeeperDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
